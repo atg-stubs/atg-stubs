91b08c8ec7ea03057908a5c17a5035788d7576ba
chris damour <chris.damour@vitalize.com>
2017-03-31 01:08:29 -0600

    added missing constructor to GSAAdminServlet

--- src/main/java/atg/adapter/gsa/GSAAdminServlet.java
+++ src/main/java/atg/adapter/gsa/GSAAdminServlet.java
@@ -1,6 +1,19 @@
 package atg.adapter.gsa;
 
+import atg.nucleus.Nucleus;
+import atg.nucleus.logging.ApplicationLogging;
 import atg.repository.content.ContentRepositoryAdminServlet;
 
+import javax.transaction.TransactionManager;
+
 public class GSAAdminServlet extends ContentRepositoryAdminServlet {
+
+    public GSAAdminServlet(
+        Object service,
+        ApplicationLogging logger,
+        Nucleus nucleus,
+        TransactionManager txManager
+    ) {
+        super(service, logger, nucleus, txManager);
+    }
 }
