2eefa8e1e6fad04fbff1cfb52e44b2ab9a3169d3
drdamour <drdamour@gmail.com>
2017-03-31 01:42:45 -0600

    add getItemDescriptorNames

--- src/main/java/atg/repository/Repository.java
+++ src/main/java/atg/repository/Repository.java
@@ -1,4 +1,9 @@
 package atg.repository;
 
 public interface Repository extends RepositoryViewContainer {
+
+    /**
+     * @return the names of the item descriptors (the item types) in this repo
+     */
+    String[] getItemDescriptorNames();
 }
