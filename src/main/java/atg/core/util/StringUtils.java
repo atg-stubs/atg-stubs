a670b7762bc3ecfc4e4a9dfdc9844a415b913c68
drdamour <drdamour@gmail.com>
2017-04-11 12:17:06 -0600

    stub out escapeHtmlString...not sure what to do about static methods.

---
+++ src/main/java/atg/core/util/StringUtils.java
@@ -0,0 +1,11 @@
+package atg.core.util;
+
+public class StringUtils {
+
+    public static String escapeHtmlString(String string){
+        //TODO: Throwing exceptions in static methods would cause tests to fail
+        //TODO: so need to decide what should we do in them?  kinda like constructors...
+        //but harder to mock/override in unit tests
+        return string;
+    }
+}
